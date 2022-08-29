c07c85b6c7042efc539da100167ff45e4fbd3a50
Mallik k <102137113+arjun-jar@users.noreply.github.com>
2022-08-29 23:53:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,38 @@
-var stateObject = {
-    "API 650": {
-        "Seventh and Later": [["Butt"],["basic standard","Annex A spot Rt","Annex A no Rt"]],
-        "First to Sixth": [["Butt"],["Basic standard","Annexes D and G"]]
-    },
-    "API 12C": {
-        "14th and 15th ": [["Butt"],["N/A"]],
-        "3rd to 13th": [["Butt_c", "Lap_a"],["3/8 in.max. t","N/A"]],
-        "First and Second": [["Lap_a", "Lap_b", "Butt_c"],["7/16 in.max. t","1/4 in.max. t","N/A"]],
-    },
-    "Unknown": {
-        "N/A": [["Lap_a", "Lap_b","Butt","Lap_d"],["7/16 in.max. t","1/4 in.max. t","N/A"]]
-    }
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAdio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.playBtn = document.querySelector(".play");
+    this.index = 0;
+    this.bpm = 150;
+  }
+  activepad() {
+    console.log(this);
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out`;
+    });
+    this.index++;
+    if (this.index === 8) this.index = 0;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
 }
+const drumKit = new DrumKit();
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activepad);
+});
 
-var jointEfficiency =function(){
-    if(standard.value.length==0)return
-    if(edition.value.length==0)return
-    if(typeOfJoint.value.length==0)return
-    if(applicable.value.length==0)return 
-    if(applicable.value==="basic standard"&&edition.value==="Seventh and Later")document.getElementById("answer").value = "1";
-    if(applicable.value==="Annex A spot Rt"&&edition.value==="Seventh and Later")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="Annex A no Rt")document.getElementById("answer").value = "0.7";
-    if(applicable.value==="Basic standard"&&edition.value==="First to Sixth")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="Annexes D and G")document.getElementById("answer").value = "1";
-    if(applicable.value==="N/A")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="3/8 in.max. t")document.getElementById("answer").value = "0.75";
-    if(applicable.value==="7/16 in.max. t")document.getElementById("answer").value = "0.7";
-    if(applicable.value==="1/4 in.max. t")document.getElementById("answer").value = "0.55";
-    if(applicable.value==="N/A"&&edition.value=="N/A")document.getElementById("answer").value = "0.7";
-    if(typeOfJoint.value=="Lap_d"&&applicable.value==="N/A")document.getElementById("answer").value = "0.35";
- 
-}
-
-window.onload = function(){
-    var standard = document.getElementById("standard"),
-        edition = document.getElementById("edition"),
-        typeOfJoint = document.getElementById("typeOfJoint");
-        applicable= document.getElementById("applicable");
-    for (let std in stateObject) {
-        standard.options[standard.options.length] = new Option(std, std);
-    }
-    standard.onchange = function(){
-        edition.length = 1;
-        typeOfJoint.length = 1;
-        if(this.selectedIndes < 1) return;
-        for(let edyr in stateObject[this.value]){
-            edition.options[edition.options.length] = new Option(edyr, edyr);
-        }
-    }
-    edition.onchange = function(){
-        typeOfJoint.length = 1;
-        if(this.selectedIndex < 1) return;
-        var type = stateObject[standard.value][this.value][0];
-        for(let i = 0; i < type.length; i++){
-            typeOfJoint.options[typeOfJoint.options.length] = new Option(type[i], type[i]);
-        }
-    }
-    
-    typeOfJoint.onchange=function(){
-        applicable.length = 1;
-        var type = stateObject[standard.value][edition.value][1];
-        for(let i = 0; i < type.length; i++){
-            applicable.options[applicable.options.length] = new Option(type[i], type[i]);
-        }
-    }
-
-  
-
-}
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
