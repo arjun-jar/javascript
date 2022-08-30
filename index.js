f5f1ab5cb4a1c82055c388416c2d6784dc5db0de
Mallik k <102137113+arjun-jar@users.noreply.github.com>
2022-08-30 20:46:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,146 @@
-var stateObject = {
-    "API 650": {
-        "Seventh and Later": [["Butt"],["basic standard","Annex A spot Rt","Annex A no Rt"]],
-        "First to Sixth": [["Butt"],["Basic standard","Annexes D and G"]]
-    },
-    "API 12C": {
-        "14th and 15th ": [["Butt"],["N/A"]],
-        "3rd to 13th": [["Butt_c", "Lap_a"],["3/8 in.max. t","N/A"]],
-        "First and Second": [["Lap_a", "Lap_b", "Butt_c"],["7/16 in.max. t","1/4 in.max. t","N/A"]],
-    },
-    "Unknown": {
-        "N/A": [["Lap_a", "Lap_b","Butt","Lap_d"],["7/16 in.max. t","1/4 in.max. t","N/A"]]
-    }
-}
-
-var jointEfficiency =function(){
-    if(standard.value.length==0)return
-    if(edition.value.length==0)return
-    if(typeOfJoint.value.length==0)return
-    if(applicable.value.length==0)return 
-    if(applicable.value==="basic standard"&&edition.value==="Seventh and Later")document.getElementById("answer").value = "1";
-    if(applicable.value==="Annex A spot Rt"&&edition.value==="Seventh and Later")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="Annex A no Rt")document.getElementById("answer").value = "0.7";
-    if(applicable.value==="Basic standard"&&edition.value==="First to Sixth")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="Annexes D and G")document.getElementById("answer").value = "1";
-    if(applicable.value==="N/A")document.getElementById("answer").value = "0.85";
-    if(applicable.value==="3/8 in.max. t")document.getElementById("answer").value = "0.75";
-    if(applicable.value==="7/16 in.max. t")document.getElementById("answer").value = "0.7";
-    if(applicable.value==="1/4 in.max. t")document.getElementById("answer").value = "0.55";
-    if(applicable.value==="N/A"&&edition.value=="N/A")document.getElementById("answer").value = "0.7";
-    if(typeOfJoint.value=="Lap_d"&&applicable.value==="N/A")document.getElementById("answer").value = "0.35";
- 
-}
-
-window.onload = function(){
-    var standard = document.getElementById("standard"),
-        edition = document.getElementById("edition"),
-        typeOfJoint = document.getElementById("typeOfJoint");
-        applicable= document.getElementById("applicable");
-    for (let std in stateObject) {
-        standard.options[standard.options.length] = new Option(std, std);
-    }
-    standard.onchange = function(){
-        edition.length = 1;
-        typeOfJoint.length = 1;
-        if(this.selectedIndes < 1) return;
-        for(let edyr in stateObject[this.value]){
-            edition.options[edition.options.length] = new Option(edyr, edyr);
-        }
-    }
-    edition.onchange = function(){
-        typeOfJoint.length = 1;
-        if(this.selectedIndex < 1) return;
-        var type = stateObject[standard.value][this.value][0];
-        for(let i = 0; i < type.length; i++){
-            typeOfJoint.options[typeOfJoint.options.length] = new Option(type[i], type[i]);
-        }
-    }
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-audio");
+    this.snareAdio = document.querySelector(".snare-audio");
+    this.hihatAudio = document.querySelector(".hihat-audio");
+    this.playBtn = document.querySelector(".play");
+    this.selects = document.querySelectorAll("select");
+    this.mute = document.querySelectorAll(".mute")
+    this.index = 0;
+    this.bpm = 300;
+    this.isPlay=null
+  }
+  activepad() {
+    this.classList.toggle("active");
     
-    typeOfJoint.onchange=function(){
-        applicable.length = 1;
-        var type = stateObject[standard.value][edition.value][1];
-        for(let i = 0; i < type.length; i++){
-            applicable.options[applicable.options.length] = new Option(type[i], type[i]);
-        }
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if(bar.classList.contains("active")){
+      if(bar.classList.contains("kick-pad")){
+        this.kickAudio.currentTime=0
+        this.kickAudio.play();
+       
+      }
+      if(bar.classList.contains("snare-pad")){
+        this.snareAdio.currentTime=0
+        this.snareAdio.play();
+      }
+      if(bar.classList.contains("hihat-pad")){
+        this.hihatAudio.currentTime=0
+        this.hihatAudio.play();
+      }
     }
+    else{}
+    
+    });
 
-  
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+   if(!this.isPlay){
+    this.playBtn.innerText="STOP"
+     this.isPlay= setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
+    else{
+      clearInterval(this.isPlay)
+      this.isPlay=null
+      this.playBtn.innerText="PLAY"
+    }
+  }
 
+audioSelct(e){
+ if(e.target.id==="snare-select select")this.snareAdio.src=e.target.value
+ if(e.target.id==="kick-select select")this.kickAudio.src=e.target.value
+ if(e.target.id==="hihat-select select")this.hihatAudio.src=e.target.value
 }
+
+
+muteAudio(e){
+  const index=e.target.getAttribute("data-track")
+  if(index==0){
+    e.target.classList.toggle("active")
+    this.kickAudio.volume=0
+    if(!e.target.classList.contains("active")){
+      this.kickAudio.volume=1
+    }
+  }
+  if(index==1)
+  {  
+  e.target.classList.toggle("active")
+  this.snareAdio.volume=0
+  if(!e.target.classList.contains("active")){
+    this.snareAdio.volume=1
+  }
+  }
+  if(index==2)
+  {
+    e.target.classList.toggle("active")
+    this.hihatAudio.volume=0
+    if(!e.target.classList.contains("active")){
+      this.hihatAudio.volume=1
+    }
+  }
+  
+}
+toggling(){
+ const allPlads= document.querySelectorAll(".pad")
+ allPlads.forEach(pad=>{
+  pad.classList.toggle("active");
+    pad.style.color="white"
+    pad.innerText=""
+  });
+}
+addingText(name){
+  const allPlads= document.querySelectorAll(".pad")
+  allPlads.forEach(pad=>{
+    if(pad.classList.contains("active")){
+      pad.style.color="white"
+       pad.innerText=name
+    }
+    });
+  
+}
+}
+const drumKit = new DrumKit();
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activepad);
+  pad.addEventListener("animationend",function(){
+    this.style.animation=""
+  })
+});
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
+drumKit.selects.forEach(select=>{
+  select.addEventListener("change",(e)=>{
+    drumKit.audioSelct(e)
+  })
+ 
+})
+drumKit.mute.forEach(mute=>{
+ mute.addEventListener("click",function(e){
+  drumKit.muteAudio(e);
+ })
+ 
+})
+drumKit.playBtn.style="font-weight:bold;"
+
+
+
+document.querySelector(".toggling").addEventListener("click",function(){
+  drumKit.toggling()
+})
+ 
+document.querySelector(".inputs").addEventListener("input",function(e){
+ drumKit.addingText(e.target.value)
+})
+ 
